ea1bf9363cbc200e7d29b7e065c54e2651e604bf
Armend Veseli <60117923+armendveseli@users.noreply.github.com>
2022-05-15 21:14:22 -0400

    Update autoboring.py

--- source/autoboring.py
+++ source/autoboring.py
@@ -1,1 +1,28 @@
+def is_number(_object: object) -> bool:
+  if isinstance(_object, int) or isinstance(_object, float):
+    return True
+  else:
+    return False
+  
 
+def is_container(_object: object) -> bool:
+  if isinstance(_object, tuple):
+    return True
+  elif isinstance(_object, list):
+    return True
+  elif isinstance(_object, dict):
+    return True
+  elif isinstance(_object, set):
+    return True
+  else:
+    return False
+  
+
+def clamp(value, _min, _max) -> Number:
+  if is_number(value):
+    if value < _min:
+      return _min
+    if value > _max:
+      return _max
+    else:
+      return value
